2d83af7d0b8cdaebf596ee69f49e1442b7635d00
James Abley <james.abley@gmail.com>
2013-06-03 21:37:06 +0100

    Initial revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DmExplorer::Application.config.secret_token = 'a7ee4e14fed7d8fc96616331c69b926773d936f8a407560bb129967feb840eecbf1725fd0a4103f22dcc3324373bb0b531edcebe82cacd39247680fa45b79edb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DmExplorer::Application.config.session_store :cookie_store, key: '_dm-explorer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DmExplorer::Application.config.session_store :active_record_store
